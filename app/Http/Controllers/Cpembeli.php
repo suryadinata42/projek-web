e32bfb6e5e800fb2183e87fa770547e2b037da84
RipaSandi69 <suryadinataripa@gmail.com>
2026-03-31 21:42:30 +0800

    pembeli

--- app/Http/Controllers/Cpembeli.php
+++ app/Http/Controllers/Cpembeli.php
@@ -10,4 +10,50 @@ class Cpembeli extends Controller
         $pembeli = Mpembeli::all();
         return view("pembeli.tampilan",compact("pembeli"));
     }
+    public function tambah()
+    {
+    return view('pembeli.tambah');
+    }
+    public function simpan(Request $request)
+    {
+        $request->validate([
+            'id_pembeli'=> 'required|string|max:15|unique:pembeli,id_pembeli',
+            'nama' => 'required|regex:/^[\pL\s]+$/u',
+        ]);
+
+        $pembeli = new Mpembeli();
+        $pembeli->id_pembeli	= $request->id_pembeli;
+        $pembeli->nama	        = $request->nama;
+        $pembeli->jenis_kelamin   = $request->jenis_kelamin;
+        $pembeli->alamat	    = $request->alamat;
+        $pembeli->kode_pos	    = $request->kode_pos;
+        $pembeli->tanggal_lahir	= $request->tanggal_lahir;
+        $pembeli->save();
+
+        return redirect()->route('pembeli.tampilan')->with('Berhasil', 'Berhasil tersimpan');
+    }
+    public function ubah($id)
+    {
+        $pembeli = Mpembeli::findOrFail($id);
+        return view('pembeli.ubah', compact('pembeli'));
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama_pembeli'  => 'required|regex:/^[\pL\s]+$/u',
+        ]);
+
+        $pembeli = Mpembeli::findOrFail($id);
+
+        $pembeli->nama 	        = $request->nama;
+        $pembeli->jenis_kelamin	= $request->jenis_kelamin;
+        $pembeli->alamat	    = $request->alamat;
+        $pembeli->kode_pos	    = $request->kode_pos;
+        $pembeli->tanggal_lahir	  = $request->tanggal_lahir;
+        $pembeli->save();
+
+    return redirect()->route('pembeli.tampilkan')->with('Sukses', 'Berhasil tersimpan');
+    }
+
+
 }
---
+++ resources/views/pembeli/tampilan.blade.php
@@ -0,0 +1,44 @@
+<a href="{{ route('pembeli.tambah') }}">Tambah Data</a>
+<table style="width:100%">
+	<thead>
+ 		<tr>
+            <td>No</td>
+            <td>Id Pembeli</td>
+            <td>Nama Pembeli</td>
+            <td>Jenis Kelamin</td>
+            <td>Alamat</td>
+            <td>Kode Pos</td>
+            <td>Tanggal Lahir</td>
+            <td>Aksi</td>
+     	</tr>
+ 	</thead>
+    <tbody>
+  		@foreach($pembeli as $d)
+  		<tr>
+     	    <td>{{ $loop->iteration }}</td>
+            <td>{{ $d->id_pembeli }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->jenis_kelamin }}</td>
+            <td>{!! nl2br($d->alamat) !!}</td>
+            <td>{{ $d->kode_pos }}</td>
+            <td>{{ $d->tanggal_lahir }}</td>
+            <td>
+            <form action="{{ route('pembeli.hapus', $d->id) }}" method="POST" 
+				onsubmit="return confirm('Yakin mau menghapus data ini?');">
+          		@csrf
+                @method('DELETE')
+                <a href="{{ route('pembeli.ubah', $d->id) }}">Edit</a>
+                <button type="submit" class="btn btn-danger">
+                	    Hapus
+             	</button>
+          		</form>
+        	</td>
+    		</tr>
+       @endforeach
+	</tbody>
+</table>
+@if(session('success'))
+<script>
+alert("{{ session('success') }}");
+</script>
+@endif
